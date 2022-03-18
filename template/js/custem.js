ec356959ccfed2e38049f2de78b41f7b5f43ec63
Quynhdz19 <lexuanquynh0123@gmail.com>
2022-03-18 09:25:37 +0700

    file raw

---
+++ template/js/custem.js
@@ -0,0 +1,15 @@
+$('.slide').owlCarousel({
+    loop:true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    
+    
+})
+// $('.box_customer').owlCarousel({
+//     loop:true,
+//     items: 3,
+//     autoplayTimeout: 5000,
+    
+    
+// })
