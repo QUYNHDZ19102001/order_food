794476a2a9688258bff753f48929d48558b7633e
vanadi <vinh07102002@gmail.com>
2022-03-19 18:24:00 +0700

    second cmm

--- template/js/custem.js
+++ template/js/custem.js
@@ -4,12 +4,26 @@ $('.slide').owlCarousel({
     autoplay: true,
     autoplayTimeout: 5000,
     
-    
+    nav : true, // Show next and prev buttons
+    slideSpeed : 1000,
+    paginationSpeed : 2000,
+    singleItem:true
+
 })
-// $('.box_customer').owlCarousel({
-//     loop:true,
-//     items: 3,
-//     autoplayTimeout: 5000,
-    
-    
-// })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
